a6787b76ad518f1c0aaae3afd78e3d3e7ff21ec3
Karel Parkkola <karel.parkkola@gmail.com>
2022-11-15 16:49:28 +0200

    Added budget bar

--- lib/budget/budget_page.dart
+++ lib/budget/budget_page.dart
@@ -13,6 +13,7 @@ class BudgetPage extends StatefulWidget {
 
 class _BudgetPageState extends State<BudgetPage> {
   late final double screenHeight;
+  late final double screenWidth;
   var isInitialized = false;
 
   @override
@@ -23,6 +24,7 @@ class _BudgetPageState extends State<BudgetPage> {
           (MediaQuery.of(context).size.height -
                   MediaQuery.of(context).viewPadding.top) *
               SizesGlobal.appBarHeight;
+      screenWidth = MediaQuery.of(context).size.width;
 
       isInitialized = true;
     }
@@ -38,19 +40,54 @@ class _BudgetPageState extends State<BudgetPage> {
       child: Stack(
         alignment: Alignment.center,
         children: [
-          Column(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 20),
+            child: Column(
+              children: [
+                const Text('0'),
+                Container(
+                  color: Colors.black,
+                  height: 10,
+                  width: 2,
+                ),
+                Container(
+                  width: screenWidth * 0.9,
+                  height: screenHeight * 0.1,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.grey,
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: Stack(
+                    children: [
+                      Container(
+                        width: screenWidth * 0.9 * 0.5,
+                        height: double.infinity,
+                        color: Colors.green,
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.black,
+                  height: 10,
+                  width: 2,
+                ),
+              ],
+            ),
+          ),
+          // Floating action button
           Align(
             alignment: Alignment.bottomRight,
             child: Padding(
               padding: const EdgeInsets.only(bottom: 15, right: 15),
               child: FloatingActionButton(
+                onPressed: () {},
                 child: Icon(
                   Icons.add,
                   size: 50,
-                  color: Theme.of(context).colorScheme.onPrimary,
+                  color: Theme.of(context).colorScheme.onSecondary,
                 ),
-                isExtended: false,
-                onPressed: () {},
               ),
             ),
           ),
